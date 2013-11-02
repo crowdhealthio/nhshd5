a1224ce1a600aceb1ce6f6f297c0741548f9f301
Rachel Graves <rachel.graves@moneyadviceservice.org.uk>
2013-11-02 16:27:13 +0000

    Return venue data by id

--- nhsrainbow/app/models/place.rb
+++ nhsrainbow/app/models/place.rb
@@ -5,6 +5,11 @@ class Place < ActiveRecord::Base
   @client_id     = 'MIWYYAX3URRPFV3OJTT041F5QIIE1E5GRDNAQ0CLFACR5GHS'
   @client_secret = 'SPQB2PD13XMQVTWT0KFJGORE2OBBOARCCPR5RI0DT0JTSVBI'
 
+  def self.find_from_foursquare_by_id(id)
+    client = Foursquare2::Client.new(:client_id => @client_id, :client_secret => @client_secret)
+    venue = client.venue(id)
+  end
+
   def self.find_from_foursquare(lat = nil, long = nil)
     client = Foursquare2::Client.new(:client_id => @client_id, :client_secret => @client_secret)
 
@@ -17,7 +22,6 @@ class Place < ActiveRecord::Base
 
       locations << venue if valid_category?(venue.categories.first)
     end
-
     locations
   end
 
