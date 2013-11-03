64298bea83f1807a94dbf59e97338e3e5a58a910
Rachel Graves <rachel.graves@moneyadviceservice.org.uk>
2013-11-03 12:26:55 +0000

    Tag stuff

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131103100301) do
+ActiveRecord::Schema.define(:version => 20131103122315) do
 
   create_table "nhs_types", :force => true do |t|
     t.string   "name"
@@ -30,6 +30,13 @@ ActiveRecord::Schema.define(:version => 20131103100301) do
     t.datetime "updated_at",    :null => false
     t.string   "foursquare_id"
     t.string   "nhs_id"
+  end
+
+  create_table "tags", :force => true do |t|
+    t.integer  "place_id"
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "tips", :force => true do |t|
